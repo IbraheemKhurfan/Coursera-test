d605d02d3db58ba53201f30467c43df49b927b1e
IbraheemKhurfan <ibraheemkhurfan@gmail.com>
2022-11-01 22:33:23 +0300

    Testing

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println(add());
+    }
+
+    public static int add() {
+        int a = 1;
+        int b = 2;
+        return a + b;
+    }
+}
