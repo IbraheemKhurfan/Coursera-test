f1b90e2b8dce951b5940334332acefa606336fbc
Ibrahim Khurfan <ibraheemkhurfan@gmail.com>
2022-11-02 13:56:15 +0300

    Hello3

--- Test.java
+++ Test.java
@@ -1,12 +1,20 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println(add());
+        //System.out.println(add());
+        System.out.println(add(2,5));
+
     }
 
-    public static int add() {
-        int a = 1;
-        int b = 2;
-        return a + b;
-    }
+    // public static int add() {
+    //     int a = 1;
+    //     int b = 2;
+    //     return a + b;
+    // }
+
+    public static int add(int num1,int num2 ) {
+            int a = num1;
+            int b = num2;
+            return a + b;
+        }
 }
